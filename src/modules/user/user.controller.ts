3376ce5ac5c0a1e227c378bc3e831662f1218af5
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-06-23 15:13:21 +0600

    error-handling

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,20 +3,30 @@ import httpStatus from "http-status";
 import { userService } from "./user.service";
 
 const registerUser = async (req: Request, res: Response) => {
-  const payload = req.body;
+  try {
+    const payload = req.body;
 
-  const user = await userService.registerUserIntoDB(payload);
+    const user = await userService.registerUserIntoDB(payload);
 
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    status: httpStatus.CREATED,
-    message: "User registered successfully",
-    data: {
-        user
-    }
-  });
-}
+    res.status(httpStatus.CREATED).json({
+      success: true,
+      status: httpStatus.CREATED,
+      message: "User registered successfully",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        success : false,
+        status : httpStatus.INTERNAL_SERVER_ERROR,
+        message : "Failed to register user",
+        error : (error as Error).message
+    })
+  }
+};
 
 export const userController = {
-    registerUser
-}
+  registerUser,
+};
