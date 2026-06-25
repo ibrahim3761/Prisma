3c390865c391dcbee8ccfb814d1a5eae346ae9a2
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-06-25 13:27:06 +0600

    acr and rft in cookie

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,13 +7,26 @@ import httpStatus from "http-status";
 const loginUser = catchAsync( async(req : Request, res : Response, next : NextFunction)=>{
     const paylaod = req.body;
 
-    const loginResult = await authService.loginUser(paylaod)
+    const {accessToken, refreshToken} = await authService.loginUser(paylaod)
+
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 // 24 hours or 1 day  
+    })
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none",
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+    })
 
     sendResponse(res,{
       success: true,
       statusCode: httpStatus.OK,
       message: "User logged in successfully",
-      data : {loginResult}
+      data : {accessToken, refreshToken}
     })
 
 })
