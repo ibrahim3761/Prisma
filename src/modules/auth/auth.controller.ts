e06afd57d6f9021f4f10a0641a2edead2383f0d9
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-06-27 22:59:01 +0600

    refresh-token added

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,33 +4,52 @@ import { authService } from "./auth.service";
 import { sendResponse } from "../../utitils/sendResponse";
 import httpStatus from "http-status";
 
-const loginUser = catchAsync( async(req : Request, res : Response, next : NextFunction)=>{
+const loginUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const paylaod = req.body;
 
-    const {accessToken, refreshToken} = await authService.loginUser(paylaod)
+    const { accessToken, refreshToken } = await authService.loginUser(paylaod);
 
     res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "none",
-        maxAge: 1000 * 60 * 60 * 24 // 24 hours or 1 day  
-    })
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24, // 24 hours or 1 day
+    });
     res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "none",
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-    })
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    });
 
-    sendResponse(res,{
+    sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: "User logged in successfully",
-      data : {accessToken, refreshToken}
-    })
+      data: { accessToken, refreshToken },
+    });
+  },
+);
 
-})
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const {accessToken} = await authService.refreshToken(refreshToken);
+
+    sendResponse(res,{
+        success : true,
+        statusCode: httpStatus.OK,
+        message : "Token Refreshed Successfully!!",
+        data : {
+            accessToken
+        }
+    })
+  },
+);
 
 export const authController = {
-    loginUser
-}
+  loginUser,
+  refreshToken,
+};
