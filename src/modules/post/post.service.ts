f4caf18e73dca8e74030b1c9d35752a7747db659
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-06-30 13:46:58 +0600

    optimized filtering-searching-sorting

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -6,6 +6,7 @@ import {
   IPostQuery,
   IUpdatePostPaylaod,
 } from "./post.interface";
+import { PostWhereInput } from "../../../generated/prisma/models";
 
 const createPost = async (payload: ICreatePostPayload, userId: string) => {
   const result = await prisma.post.create({
@@ -21,13 +22,79 @@ const createPost = async (payload: ICreatePostPayload, userId: string) => {
 const getAllPosts = async (query: IPostQuery) => {
   const limit = query.limit ? Number(query.limit) : 10;
   const page = query.page ? Number(query.page) : 1;
-  const skip = (page-1) * limit
+  const skip = (page - 1) * limit;
 
   const sortBy = query.sortBy ? query.sortBy : "createdAt";
   const sortOrder = query.sortOrder ? query.sortOrder : "desc";
+
+  const tags = query.tags ? JSON.parse(query.tags as string) : null
+
+  const tagsArray = Array.isArray(tags) ? tags : []
+
+  console.log(tagsArray);
+  
+
+  const andConditions: PostWhereInput[] = [];
+
+  if (query.searchTerm) {
+    andConditions.push({
+      OR: [
+        {
+          title: {
+            contains: query.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          content: {
+            contains: query.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    });
+  }
+
+  if (query.title) {
+    andConditions.push({
+      title: query.title,
+    });
+  }
+
+  if (query.content) {
+    andConditions.push({
+      content: query.content,
+    });
+  }
+
+  if (query.authorId) {
+    andConditions.push({
+      authorId: query.authorId,
+    });
+  }
+
+  if (query.isFeatured) {
+    andConditions.push({
+      isFeatured: Boolean(query.isFeatured),
+    });
+  }
+
+  if (query.tags) {
+    andConditions.push({
+      tags: {
+        hasSome : tagsArray
+      }
+    });
+  }
+  
+  if (query.status) {
+    andConditions.push({
+      status: query.status,
+    });
+  }
+
   const posts = await prisma.post.findMany({
     // filtering / exact match without AND operator
-
     // where : {
     //   title : "My Third Post",
     //   content : "Ronaldo"
@@ -51,7 +118,6 @@ const getAllPosts = async (query: IPostQuery) => {
     // },
 
     // searching / partial match
-
     // where:{
     //   title : {
     //     contains : "ronaldo",
@@ -64,7 +130,6 @@ const getAllPosts = async (query: IPostQuery) => {
     // },
 
     // searching / partial search with OR operator
-
     // where: {
     //   OR: [
     //     {
@@ -83,7 +148,6 @@ const getAllPosts = async (query: IPostQuery) => {
     // },
 
     // combining search (OR) and filtering (AND)
-
     // where : {
     //   //filtering & seaching combine
     //   AND:[
@@ -127,44 +191,50 @@ const getAllPosts = async (query: IPostQuery) => {
     //   content : "desc"
     // },
 
+    //dynamic searching,filtering
+    // where: {
+    //   AND: [
+    //     //search term
+    //     query.searchTerm
+    //       ? {
+    //           OR: [
+    //             {
+    //               title: {
+    //                 contains: query.searchTerm,
+    //                 mode : "insensitive"
+    //               },
+    //             },
+    //             {
+    //               content: {
+    //                 contains: query.searchTerm,
+    //                 mode : "insensitive"
+    //               },
+    //             },
+
+    //           ],
+    //         }
+    //       : {},
+
+    //     //title filtering
+    //     query.title ? { title: query.title } : {},
+
+    //     // content filtering
+    //     query.content ? { content: query.content } : {},
+    //   ],
+    // },
+
     where: {
-      AND: [
-        //search term
-        query.searchTerm
-          ? {
-              OR: [
-                {
-                  title: {
-                    contains: query.searchTerm,
-                    mode : "insensitive"
-                  },
-                },
-                {
-                  content: {
-                    contains: query.searchTerm,
-                    mode : "insensitive"
-                  },
-                },
-
-              ],
-            }
-          : {},
-
-        //title filtering
-        query.title ? { title: query.title } : {},
-
-        // content filtering
-        query.content ? { content: query.content } : {},
-      ],
+      AND: andConditions,
     },
 
-    take : limit,
-    skip : skip,
+    //dynamic pagination
+    take: limit,
+    skip: skip,
 
-    orderBy : {
+    // dynamic sorting
+    orderBy: {
       // sortBY : sortOrder
-      [sortBy] : sortOrder
-      
+      [sortBy]: sortOrder,
     },
 
     include: {
